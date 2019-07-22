723b34c15cf9e1f2717beade33efb18e65d69631
Ethan Fulcher <fulchere@msu.edu>
2019-07-22 16:20:15 -0400

    added script to monitor averages

---
+++ monitor-averages.py
@@ -0,0 +1,10 @@
+import epics
+import time
+while True:
+    time.sleep(0.1)
+    print('%.3f'%epics.caget("ACS_DIAG:AVERAGE"),end=' ')
+    print("is the average of",end=' ')
+    v = epics.caget("ACS_DIAG:BSM_D2500:AVG_WIRE")
+    print("[",'%.3f %.3f %.3f %.3f %.3f %.3f'%(v[0],v[1],v[2],v[3],v[4],v[5]),end=' ] ')
+    print("the current wire value is",end=' ')
+    print('%.3f'%epics.caget("ACS_DIAG:BSM_D2500:WIRE_RES"))
