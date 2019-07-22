586928e96c28a747258b0fc4149af1e4a67eacdb
Ethan Fulcher <fulchere@msu.edu>
2019-07-22 16:55:58 -0400

    bug fixes

--- script-update-PVs.py
+++ script-update-PVs.py
@@ -1,14 +1,15 @@
 import epics
 import time
-
+import numpy as np
 numbs = []
 for i in range(1,101):
     numbs.append((i,i/50))
-
 while True:
     itr = 1
     while itr < 100:
-        v1,v2 = numbs[itr][0],numbs[itr][1]
+        #v1,v2 = numbs[itr][0],numbs[itr][1]
+        v1 = 50 + itr*0.3 + np.random.randn()*7
+        v2 = 1 + np.random.randn()*0.1
         epics.caput("DIAG_MTCA09:PICO_CH4:AVG_RD",v1)
         epics.caput("DIAG_MTCA09:PICO_CH4:STD_RD",v2)    
         itr += 1
